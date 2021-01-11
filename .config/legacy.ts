a9f8357d91b113a39342bcd7fb9a320e8c82a83a
mfilip <marcin.filip@gmail.com>
2021-01-11 11:36:26 +0000

    sauce labs cross browser testing

---
+++ .config/legacy.ts
@@ -0,0 +1,19 @@
+import {
+  legacyDesktop,
+  legacyMobile,
+  config as sauceConfig,
+} from "@ideal-postcodes/supported-browsers";
+import * as defaults from "./config";
+
+const customLaunchers = {
+  ...legacyDesktop,
+  ...legacyMobile,
+};
+
+module.exports = (config: any): void =>
+  config.set({
+    ...sauceConfig({ testName: "Address-Finder-Bundled", defaults }),
+    browsers: Object.keys(customLaunchers),
+    customLaunchers,
+    files: ["dist/address-finder.js", { pattern: "test/umd.integration.ts" }],
+  });
