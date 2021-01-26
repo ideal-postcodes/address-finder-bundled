f539e2ab8a79421a8cbcdccad1d8ec175ab97d70
marcin <marcin.filip@gmail.com>
2021-01-26 11:45:13 +0100

    test(Builds): Test UMD, Browser and ESM builds

---
+++ cypress/support/index.ts
@@ -0,0 +1,23 @@
+import { Config } from "@ideal-postcodes/jsutil";
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      setup(url: string): void;
+    }
+  }
+}
+
+declare global {
+  interface Window {
+    apiKey: string;
+  }
+}
+
+Cypress.Commands.add("setup", (url) => {
+  cy.visit(url, {
+    onBeforeLoad: (window) => {
+      window.apiKey = Cypress.env("API_KEY") || "iddqd";
+    },
+  });
+});
