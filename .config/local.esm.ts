4a8572afba102b9d21ee12fdf6dae88889f0714f
marcin <marcin.filip@gmail.com>
2021-02-09 14:51:46 +0100

    executablePath fix

--- .config/local.esm.ts
+++ .config/local.esm.ts
@@ -9,8 +9,28 @@ import {
   preprocessors,
 } from "./config";
 
-import { executablePath } from "puppeteer";
-process.env.CHROME_BIN = executablePath();
+//import { executablePath } from "puppeteer";
+//process.env.CHROME_BIN = executablePath();
+
+import { readdirSync } from "fs";
+import { resolve } from "path";
+
+const path = "../node_modules/puppeteer/.local-chromium/";
+
+const directories = (source:string) =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+
+process.env.CHROME_BIN = resolve(__dirname, path, directories(resolve(__dirname, path))[0], "chrome-linux", "chrome");
+
+const browsers = ["ChromeHeadlessSand"];
+const customLaunchers = {
+  ChromeHeadlessSand: {
+    base: 'ChromeHeadless',
+    flags: ['--no-sandbox']
+  }
+};
 
 module.exports = (config: any): void =>
   config.set({
@@ -30,7 +50,8 @@ module.exports = (config: any): void =>
       nodeResolve: true,
       moduleDirs: ["node_modules", "dist"],
     },
-    browsers: ["ChromeHeadless"],
+    browsers,
+    customLaunchers,
     files: [
       { pattern: "test/esm.integration.js", type: "module" },
       { pattern: "dist/address-finder.esm.js", type: "module" },
--- .config/local.umd.ts
+++ .config/local.umd.ts
@@ -9,8 +9,28 @@ import {
   preprocessors,
 } from "./config";
 
-import { executablePath } from "puppeteer";
-process.env.CHROME_BIN = executablePath();
+//import { executablePath } from "puppeteer";
+//process.env.CHROME_BIN = executablePath();
+
+import { readdirSync } from "fs";
+import { resolve } from "path";
+
+const path = "../node_modules/puppeteer/.local-chromium/";
+
+const directories = (source:string) =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+
+process.env.CHROME_BIN = resolve(__dirname, path, directories(resolve(__dirname, path))[0], "chrome-linux", "chrome");
+
+const browsers = ["ChromeHeadlessSand"];
+const customLaunchers = {
+  ChromeHeadlessSand: {
+    base: 'ChromeHeadless',
+    flags: ['--no-sandbox']
+  }
+};
 
 module.exports = (config: any): void =>
   config.set({
@@ -21,7 +41,8 @@ module.exports = (config: any): void =>
     plugins: ["karma-mocha", "karma-typescript", "karma-chrome-launcher"],
     singleRun,
     basePath,
-    browsers: ["ChromeHeadless"],
+    browsers,
+    customLaunchers,
     files: [
       { pattern: "dist/address-finder.js" },
       { pattern: "test/umd.integration.ts" },
