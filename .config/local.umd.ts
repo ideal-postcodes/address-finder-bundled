9f6afd2e768e549ce71a1de46f133b5059928da8
mfilip <marcin.filip@gmail.com>
2021-06-29 09:55:19 +0200

    puppeter fix

--- .config/local.umd.ts
+++ .config/local.umd.ts
@@ -9,9 +9,6 @@ import {
   preprocessors,
 } from "./config";
 
-import { executablePath } from "puppeteer";
-process.env.CHROME_BIN = executablePath();
-
 module.exports = (config: any): void =>
   config.set({
     karmaTypescriptConfig: { compilerOptions: {} },
